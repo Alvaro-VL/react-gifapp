60b15bd720909a581ab89ec7e48a5f602ffed536
Alvaro Vecina <alvarovecinal@gmail.com>
2021-03-04 12:52:53 +0100

    Second

---
+++ docs/precache-manifest.5e6c3793c4845c573e098aeec94e6a09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5550b9327a2a45e4c8782e4c9000d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "098b908db6273ddf95b4",
+    "url": "/static/css/main.b94c1bef.chunk.css"
+  },
+  {
+    "revision": "6356afb0307b1aa1af34",
+    "url": "/static/js/2.fecf8859.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.fecf8859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098b908db6273ddf95b4",
+    "url": "/static/js/main.a1f3e53f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
